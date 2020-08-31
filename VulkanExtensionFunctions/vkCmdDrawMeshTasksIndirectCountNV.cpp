a2fe9cae926d42839d10bbfb0a2f1c9fce8ac066
Douglas Kaip <dkaip@earthlink.net>
2020-08-31 13:29:46 -0600

    Added implementation

--- VulkanExtensionFunctions/vkCmdDrawMeshTasksIndirectCountNV.cpp
+++ VulkanExtensionFunctions/vkCmdDrawMeshTasksIndirectCountNV.cpp
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <iostream>
 
 using namespace std;
 
 #include "com_CIMthetics_jvulkan_VulkanCore_NativeProxies.h"
 #include "JVulkanHelperFunctions.hh"
+#include "slf4j.hh"
 
 /*
  * Class:     com_CIMthetics_jvulkan_VulkanCore_NativeProxies
@@ -26,7 +26,41 @@ using namespace std;
  * Signature: (Lcom/CIMthetics/jvulkan/VulkanCore/Handles/VkCommandBuffer;Lcom/CIMthetics/jvulkan/VulkanCore/Handles/VkBuffer;JLcom/CIMthetics/jvulkan/VulkanCore/Handles/VkBuffer;JII)V
  */
 JNIEXPORT void JNICALL Java_com_CIMthetics_jvulkan_VulkanCore_NativeProxies_vkCmdDrawMeshTasksIndirectCountNV
-  (JNIEnv *, jobject, jobject, jobject, jlong, jobject, jlong, jint, jint)
+  (JNIEnv *env, jobject, jobject jVkCommandBuffer, jobject jVkBuffer, jlong jOffset, jobject jCountBuffer, jlong jCountBufferOffset, jint jMaxDrawCount, jint jStride)
 {
-    std::cerr << "Not implemented yet." << std::endl;
+    VkCommandBuffer_T *commandBufferHandle = (VkCommandBuffer_T *)jvulkan::getHandleValue(env, jVkCommandBuffer);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve VkCommandBuffer handle");
+        return;
+    }
+
+    VkBuffer_T *bufferHandle = (VkBuffer_T *)jvulkan::getHandleValue(env, jVkBuffer);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve VkBuffer handle");
+        return;
+    }
+
+    VkDeviceSize offset = (VkDeviceSize)jOffset;
+
+    VkBuffer_T *countBufferHandle = (VkBuffer_T *)jvulkan::getHandleValue(env, jCountBuffer);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve jCountBuffer handle");
+        return;
+    }
+
+    VkDeviceSize countBufferOffset = (VkDeviceSize)jCountBufferOffset;
+    uint32_t maxDrawCount = (uint32_t)jMaxDrawCount;
+    uint32_t stride = (uint32_t)jStride;
+
+    vkCmdDrawMeshTasksIndirectCountNV(
+    		commandBufferHandle,
+			bufferHandle,
+			offset,
+			countBufferHandle,
+			countBufferOffset,
+			maxDrawCount,
+			stride);
 }
