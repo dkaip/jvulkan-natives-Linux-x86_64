06cda6fd93704e3fde22fed86ce88a4f73feedc9
Douglas Kaip <dkaip@earthlink.net>
2020-08-31 13:18:38 -0600

    Added implementation

--- VulkanExtensionFunctions/vkCmdCopyAccelerationStructureNV.cpp
+++ VulkanExtensionFunctions/vkCmdCopyAccelerationStructureNV.cpp
@@ -13,20 +13,67 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <iostream>
 
 using namespace std;
 
 #include "com_CIMthetics_jvulkan_VulkanCore_NativeProxies.h"
 #include "JVulkanHelperFunctions.hh"
+#include "slf4j.hh"
 
 /*
  * Class:     com_CIMthetics_jvulkan_VulkanCore_NativeProxies
- * Method:    vkCmdCopyAccelerationStructureNVX
- * Signature: (Lcom/CIMthetics/jvulkan/VulkanCore/Handles/VkCommandBuffer;Lcom/CIMthetics/jvulkan/VulkanExtensions/Handles/VkAccelerationStructureNVX;Lcom/CIMthetics/jvulkan/VulkanExtensions/Handles/VkAccelerationStructureNVX;Lcom/CIMthetics/jvulkan/VulkanExtensions/Enums/VkCopyAccelerationStructureModeNVX;)V
+ * Method:    vkCmdCopyAccelerationStructureNV
+ * Signature: (Lcom/CIMthetics/jvulkan/VulkanCore/Handles/VkCommandBuffer;Lcom/CIMthetics/jvulkan/VulkanExtensions/Handles/VkAccelerationStructureKHR;Lcom/CIMthetics/jvulkan/VulkanExtensions/Handles/VkAccelerationStructureKHR;Lcom/CIMthetics/jvulkan/VulkanExtensions/Enums/VkCopyAccelerationStructureModeKHR;)V
  */
-JNIEXPORT void JNICALL Java_com_CIMthetics_jvulkan_VulkanCore_NativeProxies_vkCmdCopyAccelerationStructureNVX
-  (JNIEnv *, jobject, jobject, jobject, jobject, jobject)
+JNIEXPORT void JNICALL Java_com_CIMthetics_jvulkan_VulkanCore_NativeProxies_vkCmdCopyAccelerationStructureNV
+  (JNIEnv *env, jobject, jobject jVkCommandBuffer, jobject jCopyDestinationObject, jobject jCopySourceObject, jobject jModeObject)
 {
-    std::cerr << "Not implemented yet." << std::endl;
+    VkCommandBuffer_T *commandBufferHandle = (VkCommandBuffer_T *)jvulkan::getHandleValue(env, jVkCommandBuffer);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve VkCommandBuffer handle");
+        return;
+    }
+
+    VkAccelerationStructureKHR_T *destinationHandle = (VkAccelerationStructureKHR_T *)jvulkan::getHandleValue(env, jCopyDestinationObject);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve jCopyDestinationObject handle");
+        return;
+    }
+
+    VkAccelerationStructureKHR_T *sourceHandle = (VkAccelerationStructureKHR_T *)jvulkan::getHandleValue(env, jCopySourceObject);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not retrieve jCopySourceObject handle");
+        return;
+    }
+
+    jclass vkCopyAccelerationStructureModeKHREnumClass = env->GetObjectClass(jModeObject);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Error calling GetObjectClass for jModeObject.");
+        return;
+    }
+
+    jmethodID valueOfMethodId = env->GetMethodID(vkCopyAccelerationStructureModeKHREnumClass, "valueOf", "()I");
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not get method id for valueOf.");
+        return;
+    }
+
+    VkCopyAccelerationStructureModeKHR vkCopyAccelerationStructureModeKHR = (VkCopyAccelerationStructureModeKHR)env->CallIntMethod(jModeObject, valueOfMethodId);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Error calling CallIntMethod.");
+        return;
+    }
+
+
+    vkCmdCopyAccelerationStructureNV(
+    		commandBufferHandle,
+			destinationHandle,
+			sourceHandle,
+			vkCopyAccelerationStructureModeKHR);
 }
