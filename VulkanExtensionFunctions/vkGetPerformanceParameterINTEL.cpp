bc9eba618cb628b41b5036f3798a4eda49ebd41d
Douglas Kaip <dkaip@earthlink.net>
2020-09-01 17:21:06 -0600

    Added implementation

--- VulkanExtensionFunctions/vkGetPerformanceParameterINTEL.cpp
+++ VulkanExtensionFunctions/vkGetPerformanceParameterINTEL.cpp
@@ -39,6 +39,132 @@ JNIEXPORT jobject JNICALL Java_com_CIMthetics_jvulkan_VulkanCore_NativeProxies_v
         return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
     }
 
-	LOGERROR(env, "%s", "Not Implemented Yet.");
-    return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    jclass vkPerformanceParameterTypeINTELEnumClass = env->GetObjectClass(jVkPerformanceParameterTypeINTEL);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Error calling GetObjectClass for jVkPerformanceParameterTypeINTEL.");
+        return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    }
+
+    jmethodID valueOfMethodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "valueOf", "()I");
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Could not get method id for valueOf.");
+        return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    }
+
+    VkPerformanceParameterTypeINTEL parameter = (VkPerformanceParameterTypeINTEL)env->CallIntMethod(jVkPerformanceParameterTypeINTEL, valueOfMethodId);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Error calling CallIntMethod.");
+        return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    }
+
+    VkPerformanceValueINTEL vkPerformanceValueINTEL = {};
+
+    VkResult result = vkGetPerformanceParameterINTEL(
+    		deviceHandle,
+			parameter,
+			&vkPerformanceValueINTEL);
+
+    jclass VkPerformanceValueINTELClass = env->GetObjectClass(jVkPerformanceValueINTEL);
+    if (env->ExceptionOccurred())
+    {
+    	LOGERROR(env, "%s", "Error calling GetObjectClass for jVkPerformanceValueINTEL.");
+        return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    }
+
+    jmethodID methodId = nullptr;
+
+    if (vkPerformanceValueINTEL.type == VK_PERFORMANCE_VALUE_TYPE_UINT32_INTEL)
+    {
+        methodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "setIntData", "(I)V");
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Could not get method id for setIntData.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+
+        env->CallVoidMethod(jVkPerformanceValueINTEL, methodId, (jint)vkPerformanceValueINTEL.data.value32);
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Error calling CallVoidMethod.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+    }
+    else if (vkPerformanceValueINTEL.type == VK_PERFORMANCE_VALUE_TYPE_UINT64_INTEL)
+    {
+        methodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "setLongData", "(J)V");
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Could not get method id for setLongData.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+
+        env->CallVoidMethod(jVkPerformanceValueINTEL, methodId, (jlong)vkPerformanceValueINTEL.data.value64);
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Error calling CallVoidMethod.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+    }
+    else if (vkPerformanceValueINTEL.type == VK_PERFORMANCE_VALUE_TYPE_FLOAT_INTEL)
+    {
+        methodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "setFloatData", "(F)V");
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Could not get method id for setFloatData.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+
+        env->CallVoidMethod(jVkPerformanceValueINTEL, methodId, vkPerformanceValueINTEL.data.valueFloat);
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Error calling CallVoidMethod.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+    }
+    else if (vkPerformanceValueINTEL.type == VK_PERFORMANCE_VALUE_TYPE_BOOL_INTEL)
+    {
+        methodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "setBooleanData", "(Z)V");
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Could not get method id for setBooleanData.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+
+        env->CallVoidMethod(jVkPerformanceValueINTEL, methodId, (jboolean)vkPerformanceValueINTEL.data.valueBool);
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Error calling CallVoidMethod.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+    }
+    else if (vkPerformanceValueINTEL.type == VK_PERFORMANCE_VALUE_TYPE_STRING_INTEL)
+    {
+        methodId = env->GetMethodID(vkPerformanceParameterTypeINTELEnumClass, "setStringData", "(Ljava/lang/String;)V");
+        if (env->ExceptionOccurred())
+        {
+        	LOGERROR(env, "%s", "Could not get method id for setStringData.");
+            return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+        }
+
+		jstring stringData = env->NewStringUTF(vkPerformanceValueINTEL.data.valueString);
+
+		env->CallVoidMethod(jVkPerformanceValueINTEL, methodId, stringData);
+		if (env->ExceptionOccurred())
+		{
+			LOGERROR(env, "%s", "Failed trying execute to get methodId of stringData");
+			return nullptr;
+		}
+
+		env->DeleteLocalRef(stringData);
+    }
+    else
+    {
+    	LOGERROR(env, "Invalid type of %d in type field.", vkPerformanceValueINTEL.type);
+        return jvulkan::createVkResult(env, VK_RESULT_MAX_ENUM);
+    }
+
+    return jvulkan::createVkResult(env, result);
 }
